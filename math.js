54713230c540d368352d6d99e2f4d95e65b59383
Joshua <cutejosh2@gmail.com>
2023-04-21 02:30:16 +0100

    lesson 1 done

---
+++ math.js
@@ -0,0 +1,11 @@
+exports.add = (a, b) => a + b;
+exports.subraction = (a, b) => a - b;
+exports.multiply = (a, b) => a * b;
+exports.divide = (a, b) => a / b;
+
+// module.exports = {
+// 	add,
+// 	subraction,
+// 	multiply,
+// 	divide
+// };
