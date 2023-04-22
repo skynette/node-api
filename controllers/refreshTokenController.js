ecb16f984911e91dc80ff90bc9c1ec76f6a4cfdf
Joshua <cutejosh2@gmail.com>
2023-04-22 12:43:14 +0100

    implementing refresh token function

---
+++ controllers/refreshTokenController.js
@@ -0,0 +1,36 @@
+const usersDB = {
+	users: require('../model/users.json'),
+	setUsers: function (data) { this.users = data }
+}
+
+
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+
+const handleRereshToken = (req, res) => {
+	const cookies = req.cookies
+	if (!cookies?.jwt) return res.status(401).json({ 'error': 'Missing refresh token' });
+	console.log(cookies.jwt);
+
+	const refreshToken = cookies.jwt;
+
+	const foundUser = usersDB.users.find(u => u.refreshToken === refreshToken);
+	if (!foundUser) return res.status(401).json({ 'error': 'Invalid refresh token' }); //forbidden
+
+	jwt.verify(
+		refreshToken,
+		process.env.REFRESH_TOKEN_SECRET,
+		(err, user) => {
+			if (err|| foundUser.username !== decoded.username) return res.status(403).json({ 'error': 'Invalid refresh token' }); //forbidden
+			const accessToken = jwt.sign(
+				{ username: decoded.username },
+				process.env.ACCESS_TOKEN_SECRET,
+				{ expiresIn: '30s' }
+			)
+			res.json({ accessToken });
+		}
+	)
+}
+
+module.exports = { handleRereshToken };
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const corsOptions = require('./config/corsOptions');
 const { logger } = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler');
 const { verifyJWT } = require('./middleware/verifyJWT');
+const cookieParser = require('cookie-parser');
 const PORT = process.env.PORT || 3500;
 
 // custom middleware logger
@@ -19,6 +20,9 @@ app.use(express.urlencoded({ extended: false }));
 
 // built-in middleware for json 
 app.use(express.json());
+
+// middleware to parse cookies
+app.use(cookieParser());
 
 //serve static files
 app.use('/', express.static(path.join(__dirname, '/public')));
