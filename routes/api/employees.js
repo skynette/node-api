7a30e0edb3bf98f44f1fc86d60005e4180c78d1e
Joshua <cutejosh2@gmail.com>
2023-04-22 09:42:45 +0100

    user password and authentication

--- routes/api/employees.js
+++ routes/api/employees.js
@@ -1,15 +1,14 @@
-const express = require("express")
-const router = express.Router()
-const employeesController = require("../../controllers/employeesController")
-
+const express = require('express');
+const router = express.Router();
+const employeesController = require('../../controllers/employeesController');
 
 router.route('/')
-	.get(employeesController.getAllEmployees)
-	.post(employeesController.createNewEmployee)
-	.put(employeesController.updateEmployee)
-	.delete(employeesController.deleteEmployee);
+    .get(employeesController.getAllEmployees)
+    .post(employeesController.createNewEmployee)
+    .put(employeesController.updateEmployee)
+    .delete(employeesController.deleteEmployee);
 
-router.route("/:id")
-	.get(employeesController.getEmployeeById)
+router.route('/:id')
+    .get(employeesController.getEmployee);
 
-module.exports = router
+module.exports = router;
--- routes/root.js
+++ routes/root.js
@@ -1,10 +1,9 @@
-const express = require("express")
-const path = require("path")
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const path = require('path');
 
-router.get("^/$|/index(.html)?", (req, res) => {
-	// res.sendFile("./views/index.html", { root: __dirname })
-	res.sendFile(path.join(__dirname, "..", "views", "index.html"))
-})
+router.get('^/$|/index(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
+});
 
-module.exports = router
+module.exports = router;
