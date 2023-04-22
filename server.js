dd75cebf00855df245f5ded594ae913225d30a27
Joshua <cutejosh2@gmail.com>
2023-04-22 02:09:27 +0100

    routing in express

--- server.js
+++ server.js
@@ -32,20 +32,11 @@ app.use(express.json())
 
 // built in middleware for static files
 app.use(express.static(path.join(__dirname, "/public")))
+app.use('/subdir', express.static(path.join(__dirname, "/public")))
 
-app.get("^/$|/index(.html)?", (req, res) => {
-	// res.sendFile("./views/index.html", { root: __dirname })
-	res.sendFile(path.join(__dirname, "views", "index.html"))
-})
+app.use("/", require('./routes/root'))
+app.use('/subdir', require('./routes/subdir'))
 
-app.get("/new-page(.html)?", (req, res) => {
-	// res.sendFile("./views/index.html", { root: __dirname })
-	res.sendFile(path.join(__dirname, "views", "new-page.html"))
-})
-
-app.get("/old-page(.html)?", (req, res) => {
-	res.redirect(301, "/new-page.html")
-})
 
 // Route handlers
 app.get('/hello(.html)?', (req, res, next) => {
