f4fb4959afb778396666f9ea2c5051db11f0f1cc
Joshua <cutejosh2@gmail.com>
2023-04-22 13:55:54 +0100

    logout routes

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ app.use('/', require('./routes/root'));
 app.use('/register', require('./routes/register'));
 app.use('/login', require('./routes/auth'));
 app.use('/refresh', require('./routes/refresh'));
+app.use('/logout', require('./routes/logout'));
 
 // protected routes
 app.use(verifyJWT)
