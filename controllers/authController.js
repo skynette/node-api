dc037028079c9c8c82be123313f61f0a6c6557f1
Joshua <cutejosh2@gmail.com>
2023-04-22 12:00:39 +0100

    jwt sign tokens

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,6 +4,10 @@ const usersDB = {
 }
 
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const fsPromises = require('fs').promises;
+const path = require('path');
 
 const handleLogin = async (req, res) => {
 	const { user, pwd } = req.body;
@@ -16,7 +20,26 @@ const handleLogin = async (req, res) => {
 	// compare the password
 	const match = await bcrypt.compare(pwd, foundUser.password);
 	if (!match) return res.status(401).json({ 'error': 'Invalid username or password' });
-	res.json({ 'success': `Welcome ${user}` });
+
+	// create a jwt tokens
+	const accessToken = jwt.sign(
+		{ username: foundUser.username },
+		process.env.ACCESS_TOKEN_SECRET,
+		{ expiresIn: '30s' }
+	)
+	const refreshToken = jwt.sign(
+		{ username: foundUser.username },
+		process.env.REFRESH_TOKEN_SECRET,
+		{ expiresIn: '1d' }
+	)
+	// save the refresh token with the current user in the database
+	const otherUsers = usersDB.users.filter(u => u.username !== foundUser.username);
+	const currentUser = { ...foundUser, refreshToken };
+	usersDB.setUsers([...otherUsers, currentUser]);
+	await fsPromises.writeFile(path.join(__dirname, '../model/users.json'), JSON.stringify(usersDB.users));
+	
+	res.cookie('jwt', refreshToken, { httpOnly: true, maxAge: 24 * 60 *60 * 1000 });
+	res.json({ accessToken});
 }
 
 module.exports = { handleLogin };
