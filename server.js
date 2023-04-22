35927d353125ce6d98487b3091e04dd5fe3e7a8c
Joshua <cutejosh2@gmail.com>
2023-04-22 01:56:29 +0100

    understanding middleware and app.all

--- server.js
+++ server.js
@@ -2,11 +2,27 @@ const express = require("express")
 
 const app = express()
 const path = require('path');
-const { logger } = require("./middleware/logEvents")
+const cors = require("cors")
+const { logger } = require("./middleware/logEvents");
+const errorHandler = require("./middleware/errorHandler");
 const PORT = process.env.PORT || 3500;
 
 // custom middleware logger
 app.use(logger)
+
+// cross origin resource sharing third party middleware
+const whitelist = ['https://www.example.com', 'http://127.0.0.1:3000', 'http://localhost:3500']
+const corsOptions = {
+	origin: (origin, callback) => {
+		if (whitelist.indexOf(origin) !== -1 || !origin) {
+			callback(null, true)
+		} else {
+			callback(new Error("Not allowed by CORS"))
+		}
+	},
+	optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions))
 
 // built in middleware to hanlde urlencoded data
 app.use(express.urlencoded({ extended: false }))
@@ -39,9 +55,17 @@ app.get('/hello(.html)?', (req, res, next) => {
 	res.send("hello world")
 })
 
-app.get("/*", (req, res) => {
-	res.status(404).sendFile(path.join(__dirname, "views", "404.html"))
+app.all("*", (req, res) => {
+	res.status(404)
+	if (req.accepts('html')){
+		res.sendFile(path.join(__dirname, "views", "404.html"))
+	} else if (req.accepts('json')){
+		res.json({error: "404 not found"})
+	} else {
+		res.type('txt').send("404 not found")
+	}
 })
 
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
