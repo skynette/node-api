6a8afa2dfbd88658c62e22f07586a62b039f34ef
Joshua <cutejosh2@gmail.com>
2023-06-27 23:02:37 +0100

    fixed cors issue

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -2,13 +2,14 @@ const allowedOrigins = require('./allowedOrigins');
 
 const corsOptions = {
     origin: (origin, callback) => {
-        if (allowedOrigins.indexOf(origin) !== -1 || origin) {
-            callback(null, true)
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
         } else {
             callback(new Error('Not allowed by CORS'));
         }
     },
-    optionsSuccessStatus: 200
-}
+    optionsSuccessStatus: 200,
+    credentials: true, // Allow requests with credentials
+};
 
 module.exports = corsOptions;
